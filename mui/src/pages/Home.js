3b9598bd7b7803f81a86e54df1250ef7da9715f2
YofaGh <ghadiriyosef123@gmail.com>
2023-09-02 20:16:53 +0330

    Added homePage contents

--- mui/src/pages/Home.js
+++ mui/src/pages/Home.js
@@ -1,5 +1,7 @@
 import "./../App.css";
+import "./home.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
@@ -8,6 +10,15 @@ function HomePage() {
         <div className="App-header">
           <h1>Manga Downloader</h1>
         </div>
+        <Link to={{ pathname: "library" }}>
+          <button className="home-button library"></button>
+        </Link>
+        <Link to={{ pathname: "search" }}>
+          <button className="home-button search"></button>
+        </Link>
+        <Link to={{ pathname: "modules" }}>
+          <button className="home-button modules"></button>
+        </Link>
       </div>
     </div>
   );

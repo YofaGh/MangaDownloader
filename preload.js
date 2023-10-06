bab02d6b255dd64713433b7344a52336e50f433a
YofaGh <ghadiriyosef123@gmail.com>
2023-10-06 12:36:20 +0330

    Added new functions to preload

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer, shell } = require("electron");
 const fs = require("fs");
 
 contextBridge.exposeInMainWorld("do", {
@@ -8,11 +8,25 @@ contextBridge.exposeInMainWorld("do", {
   minimizeApp: () => {
     ipcRenderer.send("minimizeApp");
   },
+  openFolder: (path) => {
+    shell.openExternal(path);
+  },
+  createFolder: (path) => {
+    fs.mkdirSync(path, { recursive: true }, (err) => {});
+  },
+  ls: (path) => {
+    return fs.readdirSync(path);
+  },
+  removeFolder: (path) => {
+    try {
+      fs.rmSync(path, { recursive: true });
+    } catch (err) {}
+  },
   getJsonFile: (path) => {
     const fileContent = fs.readFileSync(path, "utf8");
     return JSON.parse(fileContent);
   },
   setJsonFile: (path, data) => {
-    fs.writeFileSync(path, JSON.stringify(data));
+    fs.writeFileSync(path, JSON.stringify(data, null, 2), "utf8");
   },
 });
